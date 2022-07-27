411b1edcd076c2aacf4fe607ae5d1785f5fe5c30
DevilSAM <daredevilsam@rambler.ru>
2022-07-27 12:07:27 +0300

    правки текста

--- src/js/components/SixthComponent.vue
+++ src/js/components/SixthComponent.vue
@@ -12,7 +12,7 @@
                 </div>
                 <div class="suffix">
                     <p class="section-stat right-stat">30% агентов <span class="d-sm-none text-primary">-<br>от 300 до 400 т.р. в месяц</span></p>
-                    <h2 class="stat d-sm-inline d-none">от 300 до 400 т.р.<br>в месяц</h2>
+                    <h2 class="stat d-sm-inline d-none">от 300 до 800 т.р.<br>в месяц</h2>
                 </div>
             </div>
             <p class="diagram-ps text-center section-stat">0% агентов -<br> менее 100 т.р. в месяц</p>
