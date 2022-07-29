d073b7b11975b54cbd97ea6eae4dd71f21474b53
DevilSAM <daredevilsam@rambler.ru>
2022-07-29 16:21:40 +0300

    заменил адре электронной почты в футере

--- src/js/components/FooterComponent.vue
+++ src/js/components/FooterComponent.vue
@@ -7,7 +7,7 @@
 
             <div class="contact-block ms-sm-auto">
                 <p class="contact email me-4 me-lg-5 mb-0">
-                    <a href="mailto:mail@nasledie.ru" class="text-primary">mail@nasledie.ru</a>
+                    <a href="mailto:mail@nasledie.ru" class="text-primary">info@nasledie.ru</a>
                 </p>
                 <img src="src/images/icons/phone.png" alt="телефон" class="phone-icon me-1 me-sm-3">
                 <p class="contact phone text-black mb-0">
