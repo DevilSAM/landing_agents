af3513a95859117c786c44c34d77e1bc4612fd9a
DevilSAM <daredevilsam@rambler.ru>
2022-07-29 10:29:38 +0300

    добавил анимацию shake на модалку логина

--- src/js/components/lesson/LoginComponent.vue
+++ src/js/components/lesson/LoginComponent.vue
@@ -1,8 +1,22 @@
 <template>
-    <form id="login_form" action="" @submit.prevent="onSubmit" class="mx-auto">
+    <form id="login_form" action="" @submit.prevent="onSubmit" class="mx-auto" :class="{shake: name_err || password_err}">
         <h2 class="text-center mb-5">Вход в систему</h2>
-        <input class="form-control mb-4" type="text" v-model="name" placeholder="login" required>
-        <input class="form-control mb-4" type="text" v-model="password" placeholder="password" required>
+        <input class="form-control mb-4"
+               :class="{invalid: name_err}"
+               type="text"
+               v-model="name"
+               placeholder="login"
+               @focus="name_err = false"
+               required>
+        <input class="form-control mb-4"
+               :class="{invalid: password_err}"
+               type="text"
+               v-model="password"
+               placeholder="password"
+               @focus="password_err = false"
+               required>
+
+        <p class="text-danger text-center mb-4" v-if="name_err || password_err">Неверное имя / пароль<br>Используйте demo / demo</p>
 
         <button class="btn btn-primary w-50 d-block mx-auto">Войти</button>
     </form>
@@ -14,7 +28,9 @@ export default {
 
     data: ()=>({
         name: 'demo',
-        password: 'demo'
+        password: 'demo',
+        name_err: false,
+        password_err: false,
     }),
 
     mounted() {
@@ -25,13 +41,31 @@ export default {
 
         onSubmit() {
 
+            this.checkForm()
+
             if(this.name === 'demo' && this.password === 'demo') {
                 sessionStorage['accessGranted'] = 'allowed';
                 location.href = '/lesson.html';
             } else {
                 sessionStorage['accessGranted'] = 'denied';
+                if(this.name !== 'demo'){
+                    this.name_err = true
+                }
+                if(this.password !== 'demo'){
+                    this.password_err = true
+                }
+
+                // $('form').addClass('shake-it')
             }
-        }
+        },
+
+        /**
+         * Проверка заполненности полей
+         */
+        checkForm(){
+            this.name_err = this.name !== 'demo'
+            this.password_err = this.password !== 'demo'
+        },
     }
 
 }
